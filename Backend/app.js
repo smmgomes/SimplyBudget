1856d209dc2a3428f1432dc1ddd4979e90fe435d
Jude Rozario <juderzro08@gmail.com>
2024-07-14 21:53:46 -0400

    First Commit

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_, res) => {
+    res.send("Hello World");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    mongoose
+        .connect(process.env.DB_URI)
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.log("Error: ", err.message);
+        });
+});
